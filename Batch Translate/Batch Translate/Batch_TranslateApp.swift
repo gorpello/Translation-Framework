89a4236c9dfaead6e2a5ef04e169242a3143c8f2
Gianluca Orpello <g.orpello@gmail.com>
2025-08-23 22:47:46 +0200

    Initial Commit

---
+++ Batch Translate/Batch Translate/Batch_TranslateApp.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Gianluca Orpello.
+//  Copyright © 2025 Unicorn Donkeys. All rights reserved.
+//  
+
+import SwiftUI
+
+@main
+struct Batch_TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleMessage/SimpleMessage/SimpleMessageApp.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Gianluca Orpello.
+//  Copyright © 2025 Unicorn Donkeys. All rights reserved.
+//  
+
+import SwiftUI
+
+@main
+struct SimpleMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
